5eb8a2f865c4a75c9c59a9dfcb86d69fa0a01b1c
Matthieu Bulte <matthieu.bulte.06@gmail.com>
2017-05-30 16:55:16 +0200

    panic after review

--- proposal/auto/learning_with_kernels.el
+++ proposal/auto/learning_with_kernels.el
@@ -1,28 +1,20 @@
 (TeX-add-style-hook
  "learning_with_kernels"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "latin1")))
-   (TeX-run-style-hooks
-    "paper-layout"
-    "abstract"
-    "introduction"
-    "hard_margin_svm"
-    "kernel_svm"
-    "conclusion"
-    "inputenc"
-    "mathtools"
-    "amssymb"
-    "amsmath"
-    "amsbsy"
-    "mathrsfs"
-    "color"
-    "pgfplots"
-    "tikz"
-    "relsize"
-    "graphicx")
-   (LaTeX-add-bibliographies
-    "IEEEabrv"
-    "references"))
+   (TeX-add-symbols
+    '("BIBforeignlanguage" 2)
+    '("bibinfo" 2)
+    '("url" 1)
+    "newblock"
+    "BIBentrySTDinterwordspacing"
+    "BIBentryALTinterwordstretchfactor"
+    "BIBentryALTinterwordspacing"
+    "relax"
+    "BIBdecl")
+   (LaTeX-add-bibitems
+    "Vapnik:1995:NSL:211359"
+    "Schoelkopf95extractingsupport"
+    "Decoste2002"
+    "lauer:hal"))
  :latex)
 
---
+++ proposal/auto/references.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Scholkopf:2001:LKS:559923"
+    "Shalev-Shwartz:2007:PPE:1273496.1273598"
+    "Schoelkopf95extractingsupport"
+    "Vapnik:1995:NSL:211359"
+    "lauer:hal"
+    "Decoste2002"))
+ :bibtex)
+
